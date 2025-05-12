02f54aa6bbcd6e31dc2fb485a4ff2aace60675a4
Om Jadhav <omjadhav5633@gmail.com>
2025-05-12 11:49:38 +0530

    new

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
+
+
 @app.route("/")
-def helloworld():
-    return "Hello World!"
+def home():
+    return "Hello, Flask!"
+
+
 if __name__ == "__main__":
     app.run()
